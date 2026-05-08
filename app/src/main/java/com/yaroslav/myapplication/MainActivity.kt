b156a313e7351a43f5a4969188c20366e5e5ed7c
unknown <turlay03102008@gmail.com>
2026-05-08 10:16:51 +0300

    Added movie viewmodel

--- app/src/main/java/com/yaroslav/myapplication/MainActivity.kt
+++ app/src/main/java/com/yaroslav/myapplication/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.yaroslav.myapplication.ui.theme.MovieBrowserTheme
+import com.yaroslav.myapplication.view.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +17,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieBrowserTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavigation() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieBrowserTheme {
-        Greeting("Android")
+    NavHost(
+        navController = navController,
+        startDestination = "main_screen"
+    ) {
+        composable("main_screen") {
+            MainScreen(
+                onFavoritesClick = {
+                    navController.navigate("favorites_screen")
+                }
+                // TODO перехід на деталі фільму
+            )
+        }
+
+        // TODO Екран обраного
+        composable("favorites_screen") {
+        }
     }
 }
--- app/src/main/java/com/yaroslav/myapplication/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/yaroslav/myapplication/viewmodel/MovieViewModel.kt
@@ -1,4 +1,39 @@
 package com.yaroslav.myapplication.viewmodel
 
-class MovieViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yaroslav.myapplication.BuildConfig
+import com.yaroslav.myapplication.data.Movie
+import com.yaroslav.myapplication.data.remote.RetrofitClient
+import kotlinx.coroutines.launch
+
+class MovieViewModel : ViewModel() {
+    // Стан списку фільмів
+    var movieList by mutableStateOf<List<Movie>>(emptyList())
+        private set
+
+    init {
+        fetchMovies()
+    }
+
+    private fun fetchMovies() {
+        viewModelScope.launch {
+            try {
+                val response = RetrofitClient.apiService.getPopularMovies(BuildConfig.TMDB_API_KEY)
+                movieList = response.results.map { dto ->
+                    Movie(
+                        id = dto.id,
+                        title = dto.title,
+                        overview = dto.overview,
+                        posterUrl = "https://image.tmdb.org/t/p/w500${dto.posterPath}"
+                    )
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
