2e8921e3db339a50d3dc8c7ecbec4165088b8e56
unknown <turlay03102008@gmail.com>
2026-05-08 08:48:16 +0300

    added api interface

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -48,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/yaroslav/myapplication/data/remote/MovieApiService.kt
+++ app/src/main/java/com/yaroslav/myapplication/data/remote/MovieApiService.kt
@@ -1,4 +1,23 @@
 package com.yaroslav.myapplication.data.remote
 
+import com.yaroslav.myapplication.data.MovieDto
+import com.yaroslav.myapplication.data.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface MovieApiService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String = "uk-UA"
+    ): MovieResponse
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetails(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String = "uk-UA"
+    ): MovieDto
 }
