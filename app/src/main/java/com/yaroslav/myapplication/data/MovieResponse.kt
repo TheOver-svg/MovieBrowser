ad1d4085c0affe624cfdaf5ed48a9e7af420cb1d
unknown <turlay03102008@gmail.com>
2026-05-08 10:40:13 +0300

    Added new classes for parsing movie details

--- app/src/main/java/com/yaroslav/myapplication/data/MovieResponse.kt
+++ app/src/main/java/com/yaroslav/myapplication/data/MovieResponse.kt
@@ -16,3 +16,20 @@ data class MovieDto(
     @SerialName("poster_path") val posterPath: String?,
     @SerialName("release_date") val releaseDate: String?
 )
+@Serializable
+data class MovieDetailsDto(
+    @SerialName("id") val id: Int,
+    @SerialName("title") val title: String,
+    @SerialName("overview") val overview: String,
+    @SerialName("poster_path") val posterPath: String?,
+    @SerialName("release_date") val releaseDate: String?,
+    @SerialName("vote_average") val voteAverage: Double?
+)
+
+data class MovieDetails(
+    val title: String,
+    val overview: String,
+    val posterUrl: String,
+    val releaseDate: String,
+    val rating: String
+)
