bc31a3d8e81a54ef7b5bf21468b3fa9e41ed3dff
unknown <turlay03102008@gmail.com>
2026-05-08 09:17:30 +0300

    Added data class for movie overview

---
+++ app/src/main/java/com/yaroslav/myapplication/data/Movie.kt
@@ -0,0 +1,8 @@
+package com.yaroslav.myapplication.data
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val posterUrl: String
+)
---
+++ app/src/main/java/com/yaroslav/myapplication/data/remote/RetrofitClient.kt
@@ -0,0 +1,4 @@
+package com.yaroslav.myapplication.data.remote
+
+class RetrofitClient {
+}
